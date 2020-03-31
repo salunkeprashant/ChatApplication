818f5fa0e868e1f353da8bb4d89fe32ef3e67b4c
Prashant Salunke <Prashant.Salunke@in.ey.com>
2020-03-31 23:20:19 +0530

    Unit test with assertaions

--- ChatApplication.Test/UnitTest.cs
+++ ChatApplication.Test/UnitTest.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Moq;
 using NUnit.Framework;
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ChatApplication.Test
@@ -16,21 +18,46 @@ namespace ChatApplication.Test
         [Test]
         public async Task ChatHubTestAsync()
         {
+            // Mock clients for SignalR
             Mock<IHubCallerClients> mockClients = new Mock<IHubCallerClients>();
             Mock<IClientProxy> mockClientProxy = new Mock<IClientProxy>();
+            mockClients.Setup(clients => clients.All).Returns(mockClientProxy.Object);
 
+            // Mock chat database settings
             Mock<IChatDatabaseSettings> mockChatDbSttrings = new Mock<IChatDatabaseSettings>();
-
             mockChatDbSttrings.Setup(x => x.DatabaseName).Returns("Chat");
             mockChatDbSttrings.Setup(x => x.ConnectionString).Returns("mongodb://localhost:27017");
             mockChatDbSttrings.Setup(x => x.Collections).Returns(new Collections() { UserCollectionName = "Users"});
 
-            ChatHub chatHub = new ChatHub(mockChatDbSttrings.Object);
+            // Create instance and invoke methods
+            ChatHub chatHub = new ChatHub(mockChatDbSttrings.Object) 
+            {
+                Clients = mockClients.Object
+            };
 
-            await chatHub.SendMessage(new Message());
+            // Creat and send a message
+            Message message = new Message();
+            message.message = "Hii";
+            message.date = DateTime.Today;
+            message.type = "Sent";
+            message.username = "Prashant";
+
+            await chatHub.SendMessage(message);
+
+            // Asserts, to verfiy 
+            mockClients.Verify(clients => clients.All, Times.Once);
+
+            // Note : To make following assertion work, we need to make some changes in Hub's 'SendAsync' calling method.
+            // Extension method invoke 'SendCoreAsync' internally, Since we can't mock up extension methods we need to invoke them in 
+            // such a way that it will match following arguments, e.g - SendAsync('MessageReceived' new[] {message});
+            mockClientProxy.Verify(
+                clientProxy => clientProxy.SendCoreAsync(
+                    "MessageReceived",
+                    It.Is<object[]>(item => item != null),
+                    default(CancellationToken)),
+                Times.Once);
 
 
-            mockClients.Setup(clients => clients.All).Returns(mockClientProxy.Object);
         }
     }
 }
